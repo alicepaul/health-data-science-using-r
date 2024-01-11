71b4a654320eed07e7ba483fc5129f969c0ff751
alicepaul <alice_paul@brown.edu>
2024-01-11 09:58:07 -0500

    Translate to quarto format for all chapters

--- create_refs.R
+++
@@ -1,8 +0,0 @@
-
-# generate a BibTeX database automatically for some R packages
-knitr::write_bib(c(
-  .packages(), 'tidyverse', 'gt', 'GGally', 'gtsummary', 'patchwork',
-  'broom', 'car', 'pROC', 'HDSinRdata', 'kableExtra'
-), 'packages.bib')
-
-
